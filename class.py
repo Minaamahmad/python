ac8c4be0bc1fc4b6f695d5d510607a01e458e7d5
minaamahmad@gmail.com <minaamahmad@gmail.com>
2026-09-14 17:44:37 +0500

    properties

--- class.py
+++ class.py
@@ -4,21 +4,15 @@ class Person:
         self.password = password
         print(f"Profile Created for {email}")
 
-    def get_email(self):
+    @property
+    def email(self):
             return self._email
-
-    def set_email(self, new_email):
-            if "@" in new_email:
-                self._email = new_email
-            else:
-                print("Invalid Email")
+    
 
 person1 = Person("dan@gmail.com", "danny123@")
 
-print(person1.get_email())
+print(person1.email)
 
-person1.set_email("Person@gmail.com")
 
-print(person1.get_email())
 
 
