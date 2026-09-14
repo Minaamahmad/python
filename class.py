b189b52ad0985c93a330582308c7a6c0b0151a12
minaamahmad@gmail.com <minaamahmad@gmail.com>
2026-09-14 17:53:59 +0500

    setter and getter decorator

--- class.py
+++ class.py
@@ -7,12 +7,20 @@ class Person:
     @property
     def email(self):
             return self._email
-    
+    @email.setter
+    def email(self, new_email):
+            print("Updated Email")
+            if "@" in new_email:
+                self._email = new_email
+            else:
+                print("Invalid Email")
 
 person1 = Person("dan@gmail.com", "danny123@")
 
 print(person1.email)
 
+person1.email ="123@.com"
 
+print(person1.email)
 
 
