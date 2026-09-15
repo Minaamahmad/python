3ca2037d2d312a8fdf32baffaa94d20ffb7a9587
minaamahmad@gmail.com <minaamahmad@gmail.com>
2026-09-15 20:39:45 +0500

    basic email and pass validation

--- class.py
+++ class.py
@@ -1,26 +1,39 @@
 class Person:
-    def __init__(self, email , password):
-        self._email = email
-        self.password = password
-        print(f"Profile Created for {email}")
+    def __init__(self):
+        self._email = ""
+        self.password = ""
+        print(f"Profile Created for {self.email}")
 
     @property
     def email(self):
             return self._email
+
+
+    
     @email.setter
     def email(self, new_email):
-            print("Updated Email")
-            if "@" in new_email:
+            print()
+            if "@" in new_email and ".com" in new_email:
                 self._email = new_email
             else:
                 print("Invalid Email")
 
-person1 = Person("dan@gmail.com", "danny123@")
 
-print(person1.email)
+person1 = Person()
 
-person1.email ="123@.com"
+user_email = input("Enter Email: ")
+user_pass = input("Enter Pass: ")
+person1.password = user_pass
+person1.email = user_email
 
-print(person1.email)
+if person1.email:
+     forgotpass= input("Forgot Pass Y/N: ")
+     if forgotpass == "Y":
+          new_pass = input("Enter new Passord: ")
+          person1.password= new_pass
+     if new_pass ==user_pass:
+          print("Old Password")
 
+    
 
+          
