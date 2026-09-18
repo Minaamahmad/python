76524432f875d8e9967ab012ee54a2ae129edf61
minaamahmad@gmail.com <minaamahmad@gmail.com>
2026-09-18 18:28:21 +0500

    static attributes

--- class.py
+++ class.py
@@ -1,44 +1,28 @@
-class Person:
-    def __init__(self):
-        self._email = ""
-        self.password = ""
-        print(f"Profile Created for {self.email}")
-
-    @property
-    def email(self):
-            return self._email
-
-
+class User:
+    count = 0
     
-    @email.setter
-    def email(self, new_email):
-            print()
-            if "@" in new_email and ".com" in new_email:
-                self._email = new_email
-            else:
-                print("Invalid Email")
-
-
-person1 = Person()
-
-user_email = input("Enter Email: ")
-user_pass = input("Enter Pass: ")
-person1.password = user_pass
-person1.email = user_email
-
-if person1.email:
-     forgotpass= input("Forgot Pass Y/N: ")
-     if forgotpass == "Y":
-          new_pass = input("Enter new Passord: ")
-          person1.password= new_pass
-          print("Password Changed")
-          if new_pass == person1.password:
-               print("Cannot use old password")
-     elif forgotpass =="N":
-          print("Account Created ")
-          print(f"Email: {user_email} ")
-          print(f"Password: {user_pass}")
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+        User.count+=1
         
-    
+    def display(self):
+        print(f"Username: {self.username} and Email: {self.email} ")
+        
+        
+        
+User1=User("Ali","Ali@.com")
+User2=User("dave","dave@.com")
+User3=User("bob","bob@.com")
 
-          
+User_array = []
+
+User_array.append(User1)
+User_array.append(User2)
+User_array.append(User3)
+
+
+print(f"{User.count}")
+
+for users in User_array:
+  print(users.username, users.email)
