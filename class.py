47e3ffc5db6c8269b2f40d2de92c834b65d14896
minaamahmad@gmail.com <minaamahmad@gmail.com>
2026-09-19 18:42:45 +0500

    incapsulation process

--- class.py
+++ class.py
@@ -1,28 +1,23 @@
-class User:
-    count = 0
+class BankAccount:
     
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-        User.count+=1
-        
-    def display(self):
-        print(f"Username: {self.username} and Email: {self.email} ")
+    def __init__(self,user,balance):
+        self.user = user
+        self._balance = balance
         
         
+    @property
+    def balance(self):
+        return self._balance
         
-User1=User("Ali","Ali@.com")
-User2=User("dave","dave@.com")
-User3=User("bob","bob@.com")
+    @balance.setter
+    def balance(self,amount):
+        if amount < 0:
+            raise ValueError("Insufficient Balance")
+        
+        self._balance = amount
+        
+account = BankAccount("Bob", 500)
 
-User_array = []
+account.balance += 500
 
-User_array.append(User1)
-User_array.append(User2)
-User_array.append(User3)
-
-
-print(f"{User.count}")
-
-for users in User_array:
-  print(users.username, users.email)
+print(f"{account.balance}")
