0ee068a220d7b15e9e5df65d32c36cee66b68609
minaamahmad@gmail.com <minaamahmad@gmail.com>
2026-09-21 15:33:55 +0500

    Abstraction

--- class.py
+++ class.py
@@ -1,23 +1,29 @@
-class BankAccount:
+class EmailService:  
     
-    def __init__(self,user,balance):
-        self.user = user
-        self._balance = balance
+    def _connect(self):
+         print("Connecting to server")
+            
+    def _authenticate(self , name ):
+         self.name = name
+         admin = "bob"
+         
+         if self.name != admin:
+            print("Failed to Authenticate")
+            
+            return False
+         else:
+             print("Authenticated")
+             return True
+             
+            
         
-        
-    @property
-    def balance(self):
-        return self._balance
-        
-    @balance.setter
-    def balance(self,amount):
-        if amount < 0:
-            raise ValueError("Insufficient Balance")
-        
-        self._balance = amount
-        
-account = BankAccount("Bob", 500)
+    def send_email(self):
+        self._connect()
+        if self._authenticate("bob1"):
+         print("Sending Email")
+       
 
-account.balance += 500
 
-print(f"{account.balance}")
+email = EmailService()
+
+email.send_email()
