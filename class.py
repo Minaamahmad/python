df71f5a4f8319ce44a4500417d1f7a5da65d452e
minaamahmad@gmail.com <minaamahmad@gmail.com>
2026-09-21 16:09:59 +0500

    inheritance concept

--- class.py
+++ class.py
@@ -1,29 +1,30 @@
-class EmailService:  
+class Hospital:
     
-    def _connect(self):
-         print("Connecting to server")
-            
-    def _authenticate(self , name ):
-         self.name = name
-         admin = "bob"
-         
-         if self.name != admin:
-            print("Failed to Authenticate")
-            
-            return False
-         else:
-             print("Authenticated")
-             return True
-             
-            
+    def __init__(self,name , doctor , staff):
+        self.name = name 
+        self.doctor = doctor
+        self.staff = staff
         
-    def send_email(self):
-        self._connect()
-        if self._authenticate("bob1"):
-         print("Sending Email")
-       
+    def welcome(self):
+        print(f"Welcome to {self.name}")
+        
+        
+        
+class Patient(Hospital):
+    def __init__(self, name, doctor, staff, disease , ward_number):
+        super().__init__(name, doctor, staff)
+        self.disease = disease
+        self.ward_number = ward_number
+
+class Operation(Hospital):
+    def __init__(self, name, doctor, staff, disease , Op_time):
+        super().__init__(name, doctor, staff)
+        self.disease = disease
+        self.Op_time = Op_time
 
 
-email = EmailService()
-
-email.send_email()
+patient = Patient("Private Hospital","Dr.Ahmad", "Night", "fever" , 540 )
+op_patient = Operation("Private Hospital","Dr.Ahmad", "Night", "fever" , "5:40" )
+print(patient.welcome())
+print(patient.__dict__)
+print(op_patient.__dict__)
