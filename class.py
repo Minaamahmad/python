0fd788360c226de35b67cf501b5effbfc3352750
minaamahmad@gmail.com <minaamahmad@gmail.com>
2026-09-23 12:09:17 +0500

    lambda method

--- class.py
+++ class.py
@@ -1,30 +1,5 @@
-class Hospital:
-    
-    def __init__(self,name , doctor , staff):
-        self.name = name 
-        self.doctor = doctor
-        self.staff = staff
-        
-    def welcome(self):
-        print(f"Welcome to {self.name}")
-        
-        
-        
-class Patient(Hospital):
-    def __init__(self, name, doctor, staff, disease , ward_number):
-        super().__init__(name, doctor, staff)
-        self.disease = disease
-        self.ward_number = ward_number
+array = [(2,10),(2,1),(8,3),(-2,0),(1,-4),(7,5),]
 
-class Operation(Hospital):
-    def __init__(self, name, doctor, staff, disease , Op_time):
-        super().__init__(name, doctor, staff)
-        self.disease = disease
-        self.Op_time = Op_time
-
-
-patient = Patient("Private Hospital","Dr.Ahmad", "Night", "fever" , 540 )
-op_patient = Operation("Private Hospital","Dr.Ahmad", "Night", "fever" , "5:40" )
-print(patient.welcome())
-print(patient.__dict__)
-print(op_patient.__dict__)
+sorted_array = sorted(array, key=lambda x:x[0]  )
+print(array)
+print(sorted_array)
